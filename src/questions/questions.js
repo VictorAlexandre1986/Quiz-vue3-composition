e00eee705b1c13efef14037004053a284612ddfb
Victor Alexandre <76669738+VictorAlexandre1986@users.noreply.github.com>
2024-05-13 15:30:36 -0300

    Finalizado

---
+++ src/questions/questions.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      question: 'What is the capital of France?',
+      options: ['London', 'Paris', 'Berlin', 'Rome'],
+      answer: 'Paris'
+    },
+    {
+      question: 'What is the largest planet in our solar system?',
+      options: ['Earth', 'Venus', 'Jupiter', 'Saturn'],
+      answer: 'Jupiter'
+    },
+    {
+      question: '',
+    }
+    // Add more questions here
+  ];
+  
